ce5558f7041fedce225c8d327748cebbe2cb65ce
jonghun <jonghuncu@gmail.com>
2023-03-07 21:41:27 +0900

    init project

---
+++ src/test/java/com/present/GifticBe/GifticBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.present.GifticBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifticBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
